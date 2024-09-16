5372c6c94d87179df916c6140da8ceb4694b1329
lamine <lamine@talent.etude@gmail.com>
2024-09-16 22:12:06 +0200

    add modular code

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+    <!-- Include head.html -->
+    <?php include 'components/head.html'; ?>
+</head>
+
+
+<body>
+    <!-- Include preloader.html -->
+    <?php include 'components/preloader.html'; ?>
+    
+    <!-- Include header.html -->
+    <?php include 'components/header.html'; ?>
+    
+    <!-- Include about.html -->
+    <?php include 'sections/about.html'; ?>
+    
+    <!-- Include services.html -->
+    <?php include 'sections/services.html'; ?>
+    
+    <!-- Include portfolio.html -->
+    <?php include 'sections/portfolio.html'; ?>
+    
+    <!-- Include pricing.html -->
+    <?php include 'sections/pricing.html'; ?>
+    
+    <!-- Include testimonial.html -->
+    <?php include 'sections/testimonial.html'; ?>
+    
+    <!-- Include contact.html -->
+    <?php include 'sections/contact.html'; ?>
+    
+    <!-- Include footer.html -->
+    <?php include 'components/footer.html'; ?>
+
+    <!-- Include back.html -->
+    <?php include 'components/back.html'; ?>
+
+    <!-- Include scripts.html -->
+    <?php include 'components/scripts.html'; ?>
+
+
+    
+
+</body>
+</html>
